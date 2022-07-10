51bda09b152159748968cb1032e5970a296f00d1
benno <bennokohrs@gmail.com>
2022-07-10 21:14:19 +0200

    wip config

--- packages/ui/src/components/Button.tsx
+++ packages/ui/src/components/Button.tsx
@@ -1,5 +1,6 @@
 import React from "react";
+import * as mantine from "@mantine/core";
 
 export const Button = () => {
-  return <button>Boop</button>;
+  return <mantine.Button>Jeff</mantine.Button>;
 };
