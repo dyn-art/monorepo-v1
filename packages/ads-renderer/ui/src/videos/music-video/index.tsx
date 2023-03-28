063ee24a664f9c2a5b631698ca7a0a44449f91bc
BennoDev <57860196+bennodev19@users.noreply.github.com>
2023-03-28 19:18:38 +0200

    #11 wip

--- packages/ads-renderer/ui/src/videos/music-video/index.tsx
+++ packages/ads-renderer/ui/src/videos/music-video/index.tsx
@@ -2,19 +2,63 @@ import React from 'react';
 import { Audio } from 'remotion';
 import Visualizer from './components/Visualizer';
 
-import paperTextureImage from './assets/paper-texture.png';
+import paperTexture from './assets/paper-texture.png';
+import recordCaseTexture from './assets/record-case-texture.png';
 
 const MusicVideo: React.FC<TProps> = (props) => {
+  const [color] = React.useState('#F5C26E');
   const { track, showSpotifyCode } = props;
+
   return (
-    <div>
+    <div className={'flex items-center justify-center w-full h-full'}>
+      {/* Audio */}
       <Audio src={track.previewUrl} />
-      <img
-        src={track.imageUrl}
-        className={'object-cover w-[467px] h-[467px]'}
-      />
+
+      {/* Background */}
+      <div className={'absolute top-0 left-0 w-full h-full z-0'}>
+        <div
+          style={{
+            position: 'absolute',
+            top: 0,
+            left: 0,
+            width: '100%',
+            height: '100%',
+            backgroundColor: color,
+          }}
+        />
+        <img
+          src={paperTexture}
+          style={{
+            position: 'absolute',
+            top: 0,
+            left: 0,
+            width: '100%',
+            height: '100%',
+            opacity: 0.15,
+            backgroundBlendMode: 'hard-light',
+          }}
+        />
+      </div>
+
+      <div
+        className="w-[890px] h-[890px]"
+        style={{
+          boxShadow: 'inset 0px 0px 0px 1px rgba(0, 0, 0, 0.1)',
+        }}
+      >
+        <img
+          src={track.imageUrl}
+          className={'absolute top-0 left-0 w-[890px] h-[890px]'}
+        />
+        <img
+          src={recordCaseTexture}
+          className={
+            'absolute top-0 left-0 w-[890px] h-[890px] mix-blend-difference'
+          }
+        />
+      </div>
+
       <Visualizer audioSource={track.previewUrl} />
-      <img src={paperTextureImage} width={100} height={100} />
     </div>
   );
 };
