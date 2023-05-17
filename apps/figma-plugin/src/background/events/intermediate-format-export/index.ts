25e6f3b46efb4d3b6b2b842644d06fb98d1de62d
BennoDev <57860196+bennodev19@users.noreply.github.com>
2023-05-17 17:23:04 +0200

    #17 reset top level frame node position

--- apps/figma-plugin/src/background/events/intermediate-format-export/index.ts
+++ apps/figma-plugin/src/background/events/intermediate-format-export/index.ts
@@ -1,10 +1,7 @@
 import { TBackgroundEventMeta } from '@pda/figma-handler';
 import { TBackgroundFigmaMessageEvent, logger } from '../../../shared';
 import { TBackgroundHandler } from '../../background-handler';
-import { formatNode } from './helper/format-node';
-import { stringToUint8Array } from './helper/json-to-uint8array';
-import { sha256 } from './helper/sha256';
-import { uploadDataToBucket } from './helper/upload-data-to-bucket';
+import { processNode } from './helper/process-node';
 
 export default {
   type: 'ui.message',
@@ -12,54 +9,19 @@ export default {
   callback: async (instance: TBackgroundHandler, args) => {
     logger.info(`Called Event: 'intermediate-format-export-event'`, { args });
 
-    for (const element of args.selectedElements) {
-      // Find node by id
-      const node = instance.figma.currentPage.findOne(
-        (node) => node.id === element.id
+    // Filter out unsupported nodes
+    const supportedNodes = args.selectedElements
+      .map((element) =>
+        instance.figma.currentPage.findOne((node) => node.id === element.id)
+      )
+      .filter(
+        (node) => node && ['FRAME', 'COMPONENT', 'INSTANCE'].includes(node.type)
       );
-      if (
-        node == null ||
-        (node?.type !== 'FRAME' &&
-          node.type !== 'COMPONENT' &&
-          node.type !== 'INSTANCE')
-      ) {
-        continue;
-      }
 
-      // Reset node from relative space
-      node.x = 0;
-      node.y = 0;
-      node.rotation = 0;
-      node.relativeTransform[0][2] = 0;
-      node.relativeTransform[1][2] = 0;
-
-      // Export Node
-      try {
-        const toExportNode = await formatNode(node);
-        const json = JSON.stringify(toExportNode);
-        const key = sha256(json);
-        await uploadDataToBucket(
-          key,
-          stringToUint8Array(json),
-          'application/json'
-        );
-        instance.postMessage('intermediate-format-export-result-event', {
-          type: 'success',
-          content: json,
-        });
-        figma.notify(`Successfully exported node '${node.name}'.`);
-        logger.success(`Successfully exported node '${node.name}'.`, {
-          exportedNode: toExportNode,
-          key,
-        });
-      } catch (e: any) {
-        const message = e.message ?? JSON.stringify(e);
-        instance.postMessage('intermediate-format-export-result-event', {
-          type: 'error',
-          message,
-        });
-        figma.notify(message, { error: true });
-        logger.error('Failed to export Node!', e);
+    // Process each supported node
+    for (const node of supportedNodes) {
+      if (node != null) {
+        await processNode(instance, node);
       }
     }
   },
