6999b3292fc7451535dadcf98f8fa68f3e0dbf33
BennoDev <57860196+bennodev19@users.noreply.github.com>
2023-07-12 08:34:40 +0200

    #24 fixed overlapping arc when too large radius

--- apps/web/app/components/canvas/utils/create-rectangle-path.ts
+++ apps/web/app/components/canvas/utils/create-rectangle-path.ts
@@ -24,36 +24,47 @@ export function createRectanglePath(props: {
     bottomLeftRadius,
   } = props;
 
+  // Calculate the maximum possible radius
+  const maxRadius = Math.min(width, height) / 2;
+
   return [
     // Move to the start point, considering top left radius
-    `M ${topLeftRadius} 0`,
+    `M ${Math.min(topLeftRadius, maxRadius)} 0`,
     // Draw a horizontal line to the top right corner, considering top right radius
-    `H ${width - topRightRadius}`,
+    `H ${width - Math.min(topRightRadius, maxRadius)}`,
     // Draw an arc for top right corner if radius is greater than 0
     topRightRadius > 0
-      ? `A ${topRightRadius} ${topRightRadius} 0 0 1 ${width} ${topRightRadius}`
+      ? `A ${Math.min(topRightRadius, maxRadius)} ${Math.min(
+          topRightRadius,
+          maxRadius
+        )} 0 0 1 ${width} ${Math.min(topRightRadius, maxRadius)}`
       : null,
     // Draw a vertical line to the bottom right corner, considering bottom right radius
-    `V ${height - bottomRightRadius}`,
+    `V ${height - Math.min(bottomRightRadius, maxRadius)}`,
     // Draw an arc for bottom right corner if radius is greater than 0
     bottomRightRadius > 0
-      ? `A ${bottomRightRadius} ${bottomRightRadius} 0 0 1 ${
-          width - bottomRightRadius
-        } ${height}`
+      ? `A ${Math.min(bottomRightRadius, maxRadius)} ${Math.min(
+          bottomRightRadius,
+          maxRadius
+        )} 0 0 1 ${width - Math.min(bottomRightRadius, maxRadius)} ${height}`
       : null,
     // Draw a horizontal line to the bottom left corner, considering bottom left radius
-    `H ${bottomLeftRadius}`,
+    `H ${Math.min(bottomLeftRadius, maxRadius)}`,
     // Draw an arc for bottom left corner if radius is greater than 0
     bottomLeftRadius > 0
-      ? `A ${bottomLeftRadius} ${bottomLeftRadius} 0 0 1 0 ${
-          height - bottomLeftRadius
-        }`
+      ? `A ${Math.min(bottomLeftRadius, maxRadius)} ${Math.min(
+          bottomLeftRadius,
+          maxRadius
+        )} 0 0 1 0 ${height - Math.min(bottomLeftRadius, maxRadius)}`
       : null,
     // Draw a vertical line to the top left corner, considering top left radius
-    `V ${topLeftRadius}`,
+    `V ${Math.min(topLeftRadius, maxRadius)}`,
     // Draw an arc for top left corner if radius is greater than 0
     topLeftRadius > 0
-      ? `A ${topLeftRadius} ${topLeftRadius} 0 0 1 ${topLeftRadius} 0`
+      ? `A ${Math.min(topLeftRadius, maxRadius)} ${Math.min(
+          topLeftRadius,
+          maxRadius
+        )} 0 0 1 ${Math.min(topLeftRadius, maxRadius)} 0`
       : null,
     // Close the path
     'Z',
